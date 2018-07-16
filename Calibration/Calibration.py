270a3846a29c2c7e9cd6448de5a162d68d51f1ac
Jongmin Sung <jongmin.sung@gmail.com>
2018-07-16 10:39:36 -0700

    For calibration, remove all other channels but Trap 1 and PZT.

--- Calibration/Calibration.py
+++ Calibration/Calibration.py
@@ -51,6 +51,8 @@ mHI = 2*pi*rho*R**3 # Hydrodynamic mass of bead:
 fv = nu/(pi*R**2) # Hydrodynamic frequency:
 fm = gamma_0/(2.*pi*mHI) # Frequency for inertial relaxation:
 
+ch_name = ['QPD_x', 'QPD_y', 'QPD_z', 'PZT_x', 'PZT_y', 'PZT_z']
+
 ###############################################
 
 def running_mean(x, N=1000):
@@ -89,10 +91,10 @@ class Data(object):
             self.ch[i,] = channel.data[range(N_total)]
             
         # Convert PZT unit from V to nm    
-        for i in range(3):
-            pzt = self.ch[i+8,]
+        for i in range(3, 6):
+            pzt = self.ch[i,]
             pzt = PZT_nm2V[i] * (pzt - np.mean(pzt))  
-            self.ch[i+8,] = pzt 
+            self.ch[i,] = pzt 
 
         print('Channel number = %d' % len(channels))
         print('Channel names = %s' % self.channel_name) 
@@ -106,13 +108,13 @@ class Data(object):
       
     def analyze(self):
         # PZT fit > determine A, f_drive, Axis
-        if max(self.ch[8,]) > max(self.ch[9,]):
+        if max(self.ch[3,]) > max(self.ch[4,]):
             self.os_axis = 0
         else:
             self.os_axis = 1
         
         t = dt * np.arange(N_block)
-        p_pzt, cov_pzt = curve_fit(sine, t, self.ch[self.os_axis+8,][:len(t)],  p0=[A_drive, 0, 0])
+        p_pzt, cov_pzt = curve_fit(sine, t, self.ch[self.os_axis+3,][:len(t)],  p0=[A_drive, 0, 0])
         self.pzt_A = p_pzt[0]
         self.pzt_t0 = p_pzt[1]
         self.pzt_b = p_pzt[2]
@@ -144,6 +146,11 @@ class Data(object):
         self.D2 = abs(p_psd[2])
         self.fc2 = abs(p_psd[3])      
         self.W2 = sum(P_0(f, self.D2, self.fc2))  
+        
+        print('fc1 = %d [Hz]' % (self.fc1))                              
+        print('D1 = %f [nm^2/s]' % (self.D1))
+        print('fc2 = %d [Hz]' % (self.fc2))                              
+        print('D2 = %f [nm^2/s]\n' % (self.D2))
 
         # Determine beta, kappa_fit, gamma_fit, R_fit
         self.W_th = 0.5*self.pzt_A**2 / (1 + (self.fc2 / f_drive)**2)
@@ -162,25 +169,23 @@ class Data(object):
         print('gamma = %f [pN s/nm]' % (self.gamma))
 
 
-    def plot_fig1(self):
-        n = [0, 1, 2, 8, 9, 10]
-        y = ['QPD_x (V)', 'QPD_y (V)', 'QPD_s (V)', 'PZT_x (nm)', 'PZT_y (nm)', 'PZT_z (nm)']
-        t = dt * np.arange(N_block)     # Time series in a block
+    def plot_fig1(self): # Time series 
+        t = dt * np.arange(N_block)     
         t_m = running_mean(t, int(N_drive/10))
         
         fig = plt.figure(1, figsize = (20, 10), dpi=300)  
         for i in range(6):
             sp = fig.add_subplot(6, 1, i+1)
-            ch_m = running_mean(self.ch[n[i]][:len(t)], int(N_drive/10))
+            ch_m = running_mean(self.ch[i][:len(t)], int(N_drive/10))
             sp.plot(t_m, ch_m, 'k-')
             sp.set_xlim([min(t), max(t)])               
-            sp.set_ylabel(y[i])       
+            sp.set_ylabel(ch_name[i])       
         sp.set_xlabel('Time (s)')  
    
         fig.savefig('Trace.png')
         plt.close(fig)
                                   
-    def plot_fig2(self):
+    def plot_fig2(self): # XY
         fig = plt.figure(1, figsize = (20, 10), dpi=300)  
         sp = fig.add_subplot(121)
         sp.plot(self.ch[0][:N_block], self.ch[1][:N_block], 'k.')
@@ -201,10 +206,10 @@ class Data(object):
         fig.savefig('QPD_2D.png')
         plt.close(fig)
 
-    def plot_fig3(self):
+    def plot_fig3(self): # PZT
         t = dt * np.arange(N_drive*10)
         t_m = t[::10]
-        pzt_m = self.ch[self.os_axis+8][:len(t):10]
+        pzt_m = self.ch[self.os_axis+3][:len(t):10]
         
         fig = plt.figure(1, figsize = (20, 10), dpi=300)  
         sp = fig.add_subplot(211)
@@ -215,7 +220,7 @@ class Data(object):
         sp.set_title('PZT oscillation: Data (red) vs Fit (black)')
         
         sp = fig.add_subplot(212)
-        sp.plot(t, self.ch[self.os_axis+8][:len(t)]-sine(t, self.pzt_A, self.pzt_t0, self.pzt_b), 'k')        
+        sp.plot(t, self.ch[self.os_axis+3][:len(t)]-sine(t, self.pzt_A, self.pzt_t0, self.pzt_b), 'k')        
         sp.set_xlabel('Time (s)')
         sp.set_ylabel('Residual (nm)')
         sp.set_xlim([min(t), max(t)])
@@ -223,7 +228,7 @@ class Data(object):
         fig.savefig('PZT.png')
         plt.close(fig)        
                 
-    def plot_fig4(self):
+    def plot_fig4(self): # PSD 
         f = df * np.arange(1, N_block/2)   # Freq series of a block
         fig = plt.figure(1, figsize = (20, 10), dpi=300)  
 
